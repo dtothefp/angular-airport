a99a68e280963010f583de364e8db8cdb676f2b4
David Fox Powell <dtothefp@gmail.com>
2014-05-06 10:29:30 -0400

    add gulp nodemon

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,4 +1,4 @@
 var gulp = require('gulp');
 
 // gulp.task('default', ['build', 'watch', 'serve', 'open']);
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['build', 'watch', 'develop', 'open']);
